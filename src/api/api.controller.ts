1600481ef6f0cc1ca1841fddf3695429783af882
nainglinaung <nainglinaung91@gmail.com>
2023-11-04 14:31:17 +0630

    support upload csv

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -1,10 +1,27 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CreateSearchResult } from 'src/DTO/search-result.dto';
+
 import { ApiService } from './api.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { createReadStream } from 'fs';
+import { CsvParser } from 'nest-csv-parser';
+import { CSVInputDTO } from 'src/DTO/job.dto';
 
 @Controller('search-result')
-export class SearchResultController {
-  constructor(private apiService: ApiService) {}
+export class ApiController {
+  constructor(
+    private apiService: ApiService,
+    private readonly csvParser: CsvParser,
+  ) {}
 
   @Get(':keyword')
   findByKeyword(@Param('keyword') keyword: string) {
@@ -14,6 +31,23 @@ export class SearchResultController {
   @Post('/crawl')
   crawl(@Body('keyword') keyword: string) {
     return this.apiService.crawl(keyword);
+  }
+
+  @Post('/upload-csv')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+      }),
+    }),
+  )
+  async uploadCSV(@UploadedFile() file: Express.Multer.File) {
+    const stream = createReadStream(file.path);
+    const entities = await this.csvParser.parse(stream, CSVInputDTO);
+
+    for (const data of entities.list) {
+      await this.apiService.crawl(data.keyword);
+    }
   }
 
   @Post('/')
--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ApiService } from './api.service';
 import { BullQueueModule } from 'src/bull/bull.module';
-import { SearchResultController } from './api.controller';
+import { ApiController } from './api.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { CsvModule } from 'nest-csv-parser';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
   providers: [ApiService],
   exports: [ApiService],
-  imports: [BullQueueModule, PrismaModule],
-  controllers: [SearchResultController],
+  imports: [
+    BullQueueModule,
+    PrismaModule,
+    CsvModule,
+    MulterModule.register({
+      dest: './uploads/csv',
+    }),
+  ],
+  controllers: [ApiController],
 })
 export class ApiModule {}
