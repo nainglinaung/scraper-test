ee43c1d8a5e361a59337331fb6216818a731da71
nainglinaung <nainglinaung91@gmail.com>
2023-11-07 21:20:16 +0630

    logger log

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,33 +2,27 @@ import {
   Body,
   ClassSerializerInterceptor,
   Controller,
-  Get,
   Post,
-  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDTO, RegisterDTO } from './auth.dto';
-import { AccessTokenGuard } from './AccessTokenGuard';
 
 @UseInterceptors(ClassSerializerInterceptor)
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  private readonly logger = new Logger(AuthService.name);
+  constructor(private readonly authService: AuthService) { }
 
   @Post('/login')
   async login(@Body() data: LoginDTO) {
     return this.authService.login(data);
   }
 
-  @UseGuards(AccessTokenGuard)
-  @Get('/test')
-  get() {
-    return 10;
-  }
-
   @Post('/register')
   register(@Body() credential: RegisterDTO) {
+    this.logger.log(credential);
     return this.authService.createUser(credential);
   }
 }
