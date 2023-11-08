6d129d197b9e8e460b38b323a86e206c8e8d64bc
nainglinaung <nainglinaung91@gmail.com>
2023-11-08 20:52:47 +0630

    remove exception

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { BullService } from './bull/bull.service';
-import { HttpExceptionFilter } from './common/filters/http-exception.filter'; // Replace with your filter's path
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
   const bullService = app.get(BullService);
   bullService.processJobs(); // Start processing the jobs
